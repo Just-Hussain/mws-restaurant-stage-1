73042e6bfefec934dbe0852beb52545b02d4a733
Just-Hussain <1Just.Hussain@gmail.com>
2019-07-16 13:39:25 +0300

    completed responsivene design and added a service worker for caching.

---
+++ sw.js
@@ -0,0 +1,51 @@
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  'restaurant.html',
+  'data/restaurants.json',
+  'css/styles.css',
+  'js/dbhelper.js',
+  'js/main.js',
+  'js/restaurant_info.js',
+  'img/1.jpg',
+  'img/2.jpg',
+  'img/3.jpg',
+  'img/4.jpg',
+  'img/5.jpg',
+  'img/6.jpg',
+  'img/7.jpg',
+  'img/8.jpg',
+  'img/9.jpg',
+  'img/10.jpg',
+
+];
+
+// self.addEventListener('install', function(event) {
+//   // Perform install steps
+//   event.waitUntil(
+//     caches.open(CACHE_NAME)
+//       .then(function(cache) {
+//         console.log('Opened cache');
+//         return cache.addAll(urlsToCache);
+//       })
+//   );
+// });
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('cachev1')
+    .then(function(chache) {
+      console.log('cahce opened');
+      return chache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) return response;
+      return fetch(event.request);
+    })
+  );
+})
